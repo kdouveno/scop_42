0b46b7393add4f4b18c2fbb197501cb178248fe5
kdouveno <kdouveno@student.42.fr>
2020-02-03 17:09:40 +0100

    changement a min et obj.h

--- includes/ft_obj.h
+++ includes/ft_obj.h
@@ -6,7 +6,7 @@
 /*   By: kdouveno <kdouveno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/31 15:20:01 by kdouveno          #+#    #+#             */
-/*   Updated: 2020/01/31 16:57:23 by kdouveno         ###   ########.fr       */
+/*   Updated: 2020/02/03 16:46:06 by kdouveno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,45 @@
 # include "../libs/libmatrix/includes/ft_gl.h"
 # include <stdio.h>
 
+
 typedef const char*	t_shader;
+
+typedef struct		s_face_indices{
+	unsigned int	v1;
+	unsigned int	vn1;
+	unsigned int	vt1;
+	unsigned int	v2;
+	unsigned int	vn2;
+	unsigned int	vt2;
+	unsigned int	v3;
+	unsigned int	vn3;
+	unsigned int	vt3;
+}					t_face_indices;
+
+typedef struct		s_face{
+	float			v1;
+	float			vn1;
+	float			vt1;
+	float			v2;
+	float			vn2;
+	float			vt2;
+	float			v3;
+	float			vn3;
+	float			vt3;
+}					t_face;
 
 typedef struct		s_obj
 {
-	unsigned		v_size;
-	unsigned		i_size;
+	unsigned		faces_size;
+	t_face			*faces_vbo;
 	unsigned		shader_program;
-	unsigned		voa;
 	const unsigned	*indices;
 	const float		*vertices;
 	t_mat			obj_mat;
 	t_mat			wld_mat;
 	t_shader		vertex_sh;
-	t_shader		fragment_sh; 
+	t_shader		fragment_sh;
+	char			*name;
 }					t_obj;
 
 //	PARSER {
--- srcs/main.c
+++ srcs/main.c
@@ -1,21 +1,25 @@
 #include "../includes/scop.h"
 #include "../includes/ft_glfw.h"
+#include "../includes/ft_exits.h"
 #include <stdio.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
 
-int main()
+
+int main(int argc, char **argv)
 {
-	t_obj		obj;
+	t_obj		*obj;
 	void		*win;
 
+	if (argc != 2)
+		usage();
 	ft_glfw_init();
 	if ((win = ft_glfw_new_window_context())
 		&& ft_glfw_init_glad())
 	{
-		obj = ft_parse_obj();
+		obj = ft_parse_objs(argv[1]);
 		ft_parse_shaders(&obj);
 		ft_load_shaders(&obj);
 		ft_load_objs(&obj);
